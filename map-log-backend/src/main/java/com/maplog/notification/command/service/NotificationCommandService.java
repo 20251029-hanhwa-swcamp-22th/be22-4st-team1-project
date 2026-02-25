d69d8b7aa062fa85da117e2727269d0d01820f47
wjdqudwls <wjdqudwls100@gmail.com>
2026-02-25 22:19:02 +0900

    [feat] sse 실시간 알림 기능 추가

--- map-log-backend/src/main/java/com/maplog/notification/command/service/NotificationCommandService.java
+++ map-log-backend/src/main/java/com/maplog/notification/command/service/NotificationCommandService.java
@@ -5,11 +5,14 @@ import com.maplog.common.exception.ErrorCode;
 import com.maplog.notification.command.domain.Notification;
 import com.maplog.notification.command.domain.NotificationType;
 import com.maplog.notification.command.repository.NotificationCommandRepository;
+import com.maplog.sse.SseEmitterService;
 import com.maplog.user.command.domain.User;
 import com.maplog.user.command.repository.UserCommandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -18,15 +21,18 @@ public class NotificationCommandService {
 
     private final NotificationCommandRepository notificationCommandRepository;
     private final UserCommandRepository userCommandRepository;
+    private final SseEmitterService sseEmitterService;
 
     public void createFriendRequestNotification(Long receiverId, Long friendId, String requesterNickname) {
         Notification notification = Notification.create(
                 receiverId,
                 NotificationType.FRIEND_REQUEST,
                 friendId,
-                String.format("'%s'님으로부터 새로운 친구 요청이 도착했습니다.", requesterNickname)
-        );
+                String.format("'%s'님으로부터 새로운 친구 요청이 도착했습니다.", requesterNickname));
         notificationCommandRepository.save(notification);
+        // 【SSE 실시간 푸시】친구 요청 알림을 수신자에게 즉시 전송
+        sseEmitterService.send(receiverId, "notification",
+                Map.of("type", "FRIEND_REQUEST", "message", notification.getMessage()));
     }
 
     public void createFriendAcceptedNotification(Long requesterId, Long friendId, String receiverNickname) {
@@ -34,9 +40,11 @@ public class NotificationCommandService {
                 requesterId,
                 NotificationType.FRIEND_ACCEPTED,
                 friendId,
-                String.format("'%s'님이 친구 요청을 수락했습니다.", receiverNickname)
-        );
+                String.format("'%s'님이 친구 요청을 수락했습니다.", receiverNickname));
         notificationCommandRepository.save(notification);
+        // 【SSE 실시간 푸시】친구 수락 알림을 요청자에게 즉시 전송
+        sseEmitterService.send(requesterId, "notification",
+                Map.of("type", "FRIEND_ACCEPTED", "message", notification.getMessage()));
     }
 
     public void createDiarySharedNotification(Long receiverId, Long diaryId, String diaryTitle, String sharerNickname) {
@@ -44,9 +52,11 @@ public class NotificationCommandService {
                 receiverId,
                 NotificationType.DIARY_SHARED,
                 diaryId,
-                String.format("'%s'님이 '%s' 일기를 공유했습니다.", sharerNickname, diaryTitle)
-        );
+                String.format("'%s'님이 '%s' 일기를 공유했습니다.", sharerNickname, diaryTitle));
         notificationCommandRepository.save(notification);
+        // 【SSE 실시간 푸시】일기 공유 알림을 수신자에게 즉시 전송
+        sseEmitterService.send(receiverId, "notification",
+                Map.of("type", "DIARY_SHARED", "message", notification.getMessage()));
     }
 
     public void markAsRead(String email, Long notificationId) {
